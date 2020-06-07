15e1e08e812cb3e170845b478f392d73661d35c6
jisen.liu <jisen.liu@ifchange.com>
2020-06-07 14:47:12 +0800

    test build

---
+++ doc/precache-manifest.15f9473efd60ccb52f666fb10ac58070.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17af9002ceaa86812e415ac1e1a889be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38b434f394ec4a533ed4",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2fbd4f2fcaa63f4c4877",
+    "url": "/static/js/2.dbd13957.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dbd13957.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38b434f394ec4a533ed4",
+    "url": "/static/js/main.9037a7ee.chunk.js"
+  },
+  {
+    "revision": "354c14a7f033cfde2a0f",
+    "url": "/static/js/runtime-main.fb35007a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
